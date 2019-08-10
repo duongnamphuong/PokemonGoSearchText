b54dbce5cb1cc7527ba79210e223c7a29f1260f4
Duong Nam Phuong <duongnamphuonghcm93@gmail.com>
2019-08-10 13:35:14 +0900

    SQL update: use TypeId instead of TypeName

--- tools/sql/Counters dual type Pokémon.sql
+++ tools/sql/Counters dual type Pokémon.sql
@@ -1,14 +1,15 @@
-declare @type1 varchar(12) = 'dragon',@type2 nvarchar(12) = 'flying'
+declare @Normal int = 1,@Fire int = 2,@Fighting int = 3,@Water int = 4,@Flying int = 5,@Grass int = 6,@Poison int = 7,@Electric int = 8,@Ground int = 9,@Psychic int = 10,@Rock int = 11,@Ice int = 12,@Bug int = 13,@Dragon int = 14,@Ghost int = 15,@Dark int = 16,@Steel int = 17,@Fairy int = 18
+declare @type1 int = @Dragon,@type2 int = @Flying
 
 select atk.Id,atk.Name,b.Damage
 into #t1
 from Battle b join [Type] def on b.DefenderMonoTypeId=def.Id join [Type] atk on b.AttackMoveTypeId = atk.Id
-where (def.Name=@type1)
+where (def.Id=@type1)
 
 select atk.Id,atk.Name,b.Damage
 into #t2
 from Battle b join [Type] def on b.DefenderMonoTypeId=def.Id join [Type] atk on b.AttackMoveTypeId = atk.Id
-where (def.Name=@type2)
+where (def.Id=@type2)
 
 select #t1.Id,#t1.[Name],#t1.Damage*#t2.Damage as [Damage]
 into #t3
