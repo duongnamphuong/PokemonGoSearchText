07e12c50361eceea3a72bdc1cc77bb9ff8f3c97a
Duong Nam Phuong <duongnamphuonghcm93@gmail.com>
2019-08-10 13:23:22 +0900

    SQL: TypeId.sql, NumberOfTypes.sql

---
+++ tools/sql/NumberOfTypes.sql
@@ -0,0 +1,3 @@
+select p.Id, p.FormId, p.Species, p.Atk, p.Def, p.Hp, p.CandyId, p.IsLegend, p.IsMythic, p.IsBaby, dbo.GetType(p.Id, p.FormId) as [Type], count(*) as [NumberOfTypes]
+from Pokemon p join PokeType pt on pt.PokeId = p.Id and pt.FormId = p.FormId
+group by p.Id, p.FormId, p.Species, p.Atk, p.Def, p.Hp, p.CandyId, p.IsLegend, p.IsMythic, p.IsBaby
---
+++ tools/sql/TypeId.sql
@@ -0,0 +1,1 @@
+declare @Normal int = 1,@Fire int = 2,@Fighting int = 3,@Water int = 4,@Flying int = 5,@Grass int = 6,@Poison int = 7,@Electric int = 8,@Ground int = 9,@Psychic int = 10,@Rock int = 11,@Ice int = 12,@Bug int = 13,@Dragon int = 14,@Ghost int = 15,@Dark int = 16,@Steel int = 17,@Fairy int = 18
