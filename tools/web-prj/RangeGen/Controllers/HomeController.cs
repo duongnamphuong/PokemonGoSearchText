422f5e0d9cbabc3c6beba822b8b6ececf27722e6
Duong Nam Phuong <duongnamphuonghcm93@gmail.com>
2019-08-31 00:09:52 +0900

    Cleanup some generated objects

--- tools/web-prj/RangeGen/Controllers/HomeController.cs
+++ tools/web-prj/RangeGen/Controllers/HomeController.cs
@@ -12,19 +12,5 @@ namespace RangeGen.Controllers
         {
             return View();
         }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
