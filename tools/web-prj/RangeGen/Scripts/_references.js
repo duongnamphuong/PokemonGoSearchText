c8d045d519b3080bee42b08819ff0e789e7ed33e
Duong Nam Phuong <duongnamphuonghcm93@gmail.com>
2019-09-01 22:37:17 +0900

    Install-Package jQuery -Version 3.4.1

--- tools/web-prj/RangeGen/Scripts/_references.js
+++ tools/web-prj/RangeGen/Scripts/_references.js
@@ -1,4 +1,5 @@
 ﻿/// <autosync enabled="true" />
 /// <reference path="business/number range generator.js" />
+/// <reference path="jquery-3.4.1.js" />
 /// <reference path="modernizr-2.8.3.js" />
 /// <reference path="respond.js" />
