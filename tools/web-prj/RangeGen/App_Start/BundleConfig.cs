bb209af347f25310301a322d071e9fa2213c2155
Duong Nam Phuong <duongnamphuonghcm93@gmail.com>
2019-09-01 22:01:39 +0900

    Use Site.css (but remove its content, which was auto-generated).

--- tools/web-prj/RangeGen/App_Start/BundleConfig.cs
+++ tools/web-prj/RangeGen/App_Start/BundleConfig.cs
@@ -23,9 +23,9 @@ namespace RangeGen
             //          "~/Scripts/bootstrap.js",
             //          "~/Scripts/respond.js"));
 
-            //bundles.Add(new StyleBundle("~/Content/css").Include(
+            bundles.Add(new StyleBundle("~/Content/css").Include(
             //          "~/Content/bootstrap.css",
-            //          "~/Content/site.css"));
+                      "~/Content/site.css"));
         }
     }
 }
